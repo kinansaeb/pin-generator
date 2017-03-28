396a5719ad24c9aec471a0a89d36887d17ce10f0
AbAl-Mas <AbAl-Mas@192.168.111.127>
2017-03-28 08:33:10 +0200

    bestätigungs controller angefangen

---
+++ src/view/BestätigungController.java
@@ -0,0 +1,31 @@
+package view;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class BestätigungController {
+
+	@FXML
+	private Label fingerprint;
+	@FXML
+	private Label date;
+	@FXML
+	private Label pin;
+	
+	@FXML
+	private TextField fingerprintField;
+	@FXML
+	private DatePicker dateField;
+	@FXML
+	private TextArea pinField;
+	
+	@FXML
+	private Button pinGenerate;
+	@FXML
+	private ComboBox selection;
+	
+}
